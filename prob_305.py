395b0473e47f8bb9b579de38db21fdd1c01baed5
Franky W. (franklinwahl) <wahl77+git@gmail.com>
2013-02-01 19:15:43 -0800

    Better problem 305, but not solved yet // Still no reason to ask why

--- prob_305.py
+++ prob_305.py
@@ -1,43 +1,49 @@
-looking_for = []
+word = []
+number_position = []
+word_found_times = []
 
-for i in range(0, 14):
-	looking_for.append(3**i)
+for i in range(1, 14):
+	word.append(str(3**i))
 
-print looking_for
+for i in range(0, len(word)):
+	word_found_times.append(0)
 
-
-def get_position(n):
+def get_position():
 	position = -6
-	i = 1
-	find = str(n)
-	found = 0
+	counter = 1
+	found = []
 	stream = '0000000' # Keep this 7 characters (3**13)
+
 	not_done = True
+	
+
+
+
 	while not_done:
-		for j in range(0, len(str(i))):
+		# Update Stream
+		for j in range(0, len(str(counter))): 
 			position += 1
-			stream = stream[len(str(stream))-6:]+str(i)[j]
-			found_word = True
-			#print position, stream
-			for j in range(0, len(find)):
-				if str(find)[j] != str(stream)[j]:
-			#		print "Not here: ", stream
-					found_word = False
-					break
-			if found_word == True:
-				found += 1
-				#print int(find), found
-				if found == int(find): 
-					print position, stream
-					not_done = False 
-					return position
-					break
-					
-		i += 1
+			stream = stream[len(str(stream))-6:]+str(counter)[j]
 
-print get_position(1)
-print get_position(5)
-print get_position(12)
-print get_position(7780)
+			for i in range(0, len(word)):
+				found_word = True
+				for j in range(0, len(word[i])):
+					if word[i][j] != str(stream[j]):
+						found_word = False
+						break
+				if found_word == True:
+					word_found_times[i] += 1
+					if word_found_times[i] == int(word[i]):
+						number_position.append(position)
+						print number_position
+			
+		if len(number_position) == len(word):
+			print number_position
+			return number_position
+			break
+		counter +=1
+		
 
 
+positions = get_position()
+print sum(positions)
